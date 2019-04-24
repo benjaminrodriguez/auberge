94c4dd1629cefef9967c2b695187dd6aebca0129
Reybun <simajchel@intechinfo.fr>
2019-04-24 09:00:19 +0200

    db_connect

---
+++ models/db_connect.php
@@ -0,0 +1,15 @@
+<?php
+    function bdd()
+    {
+        //CONNEXION A LA BDD
+        try
+        {
+            $bdd = new PDO('mysql:host=localhost; dbname=auberge; charset=utf8', 'root', 'toor');
+        }
+        catch (Exception $e)
+        {
+            die('Erreur : ' . $e->getMessage());
+        }
+        return $bdd;
+    }
+?>
