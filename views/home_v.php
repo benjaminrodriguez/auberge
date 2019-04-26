f641bb2c36d27effe776918dfddd6cef44eba8dd
Reybun <simajchel@intechinfo.fr>
2019-04-26 09:37:15 +0200

    benj bouton

--- views/home_v.php
+++ views/home_v.php
@@ -1,7 +1,7 @@
 <?php $title='Accueil';  ?>
 <?php ob_start(); ?>
-fefefezfzefezfezfzefzefezfezfezfkoefzefzkoefzkoezfko
 
+<a href="index.php?page=newprojet"><button class="btn btn-mini btn-inverse hidden-phone" type="button">Creer Projet</button></a>
 
 
 
