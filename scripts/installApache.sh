811af9404c11937b3eaebc9bf3af1bf36a5178a1
Benjamin Rodriguez <benjamin-rodriguez@outlook.fr>
2019-05-09 10:02:44 +0200

    scripts

--- scripts/installApache.sh
+++ scripts/installApache.sh
@@ -4,4 +4,8 @@
 # INSTALLATION APACHE
 # ------------------------------------------------
 
-
+`apt-get -y install mariadb-client mariadb-server`
+`mysql_secure_installation`
+`apt-get -y install apache2`
+`apt-get -y install php7.3 libapache2-mod-php7.3`
+`service apache2 restart`
