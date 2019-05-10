255420e274fca9eebf29adc280bcb74bd9679143
Benjamin Rodriguez <macbookpro@MacBook-Pro.local>
2019-05-10 18:25:44 +0200

    script install nodeJS

--- scripts/nodeJSInstall.sh
+++ scripts/nodeJSInstall.sh
@@ -4,9 +4,5 @@
 # INSTALLATION NodeJS
 # ------------------------------------------------
 
-`apt install -y install mariadb-server mariadb-client`
-`mysql_secure_installation`
-`apt -y install apache2`
-`apt -y install php7.3 libapache2-mod-php7.3`
-`service apache2 restart`
-`apt -y install phpmyadmin
+`curl -sL https://deb.nodesource.com/setup_11.x | bash -`
+`apt-get install -y nodejs`
