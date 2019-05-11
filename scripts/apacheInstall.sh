edae1406173754235df9469f21be0b540728884b
RODRIGUEZ Benjamin <rodriguez@intechinfo.fr>
2019-05-11 15:46:56 +0200

    apche php

--- scripts/apacheInstall.sh
+++ scripts/apacheInstall.sh
@@ -9,3 +9,10 @@
 `apt-get -y install apache2`
 `apt-get -y install php7.3 libapache2-mod-php7.3`
 `service apache2 restart`
+
+# ------------------------------------------------
+# INSTALLATION PHP
+# ------------------------------------------------
+
+`apt install -y php7.3`
+`apt install php7.3-cli php7.3-common php7.3-curl php7.3-mbstring php7.3-mysql php7.3-xml`
