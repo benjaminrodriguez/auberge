cda1bf04db35fc6d54c4af2bd47b9f0c97729b7c
Benjamin Rodriguez <benjamin-rodriguez@outlook.fr>
2019-05-11 16:33:44 +0200

    fix bug apacheInstall

--- scripts/apacheInstall.sh
+++ scripts/apacheInstall.sh
@@ -5,7 +5,7 @@
 # ------------------------------------------------
 
 `apt-get -y install mariadb-client mariadb-server`
-`mysql_secure_installation`
+#`mysql_secure_installation`
 `apt-get -y install apache2`
 `apt-get -y install php7.3 libapache2-mod-php7.3`
 `service apache2 restart`
@@ -15,4 +15,3 @@
 # ------------------------------------------------
 
 `apt install -y php7.3`
-`apt install php7.3-cli php7.3-common php7.3-curl php7.3-mbstring php7.3-mysql php7.3-xml`
