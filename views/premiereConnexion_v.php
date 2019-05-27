fcbe34592f3b48c9b2db9675be13a1fde930fb05
Reybun <simajchel@intechinfo.fr>
2019-05-27 13:35:31 +0200

    Update premiereConnexion_v.php

--- views/premiereConnexion_v.php
+++ views/premiereConnexion_v.php
@@ -1,9 +1,16 @@
 <?php $title = 'premiereConnexion'; ?>
 <?php ob_start(); ?>
 
-
-
-
+<div class="well">
+    <form enctype="multipart/form-data" action="index.php?page=firstconnect" method="POST">   
+        <input type="password" name="password" placeholder="Mot de passe"required>
+        <input type="text"  placeholder="nom" name="nom" required>
+        <input type="text"  placeholder="prenom" name="prenom" required>
+        <input type="email" placeholder="you@memocards.com" name="email" required>
+        <br>
+        <button class="btn btn-lg btn-block" name="SignIn" type="submit">S'inscrire</button>
+    </form>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 <?php require(dirname(__FILE__).'/template.php'); ?>
