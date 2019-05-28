8b90f38a26a6f9746c7aa8b63535f7b116c50445
Reybun <simajchel@intechinfo.fr>
2019-05-28 08:44:12 +0200

    create

--- controllers/create_project_c.php
+++ controllers/create_project_c.php
@@ -1,10 +1,14 @@
 <?php
 
-if (isset($_POST['nameProject']) && isset($_POST['describeProject']))
+if (isset($_POST['nameProject']) && isset($_POST['describeProject']) && isset($_POST['semestre']))
 {
     // CREATION PROJET
-
-    creation_projet_INSERT($_POST['nameProject'], $_POST['describeProject']);
+    if(!isset($_POST['entreprise'])){
+        $entreprise = "in'tech";
+    } else {
+        $entreprise = $_POST['entreprise'];
+    }
+    creation_projet_INSERT($_POST['nameProject'], $_POST['describeProject'], $entreprise, $_POST['semestre']);
 }
     require(dirname(__FILE__).'/../views/create_projet_v.php');
     require(dirname(__FILE__).'/../views/template.php');
