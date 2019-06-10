fa13dad1d4148449773474e73d080b8c913fc879
RODRIGUEZ Benjamin <rodriguez@intechinfo.fr>
2019-06-10 22:24:08 +0200

    fix bug sql

--- models/db_connect.php
+++ models/db_connect.php
@@ -4,7 +4,7 @@
         //CONNEXION A LA BDD
         try
         {
-            $bdd = new PDO('mysql:host=localhost; port=3307; dbname=auberge; charset=utf8', 'root', 'root');
+            $bdd = new PDO('mysql:host=localhost; port=3307; dbname=auberge; charset=utf8', 'root', '');
         }
         catch (Exception $e)
         {
